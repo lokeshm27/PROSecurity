57ad2605c3715be0079440567c5c67633f05c7d2
Lokesh M <lokeshm27@gmail.com>
2018-01-17 07:47:36 +0530

    Added deleteEntry method

--- KeyStorage.java
+++ KeyStorage.java
@@ -44,7 +44,7 @@ public class KeyStorage {
 	 * Initializes and Returns the KeyStore Object
 	 * If KeyStore file doesn't exits creates the new file
 	 * 
-	 * @throws: IOException if File Key is incorrect or File has been tampered
+	 * @throws IOException: if File Key is incorrect or File has been tampered
 	 */
 	private KeyStore getKeyStore() throws IOException{
 		KeyStore ks = null;
@@ -83,7 +83,6 @@ public class KeyStorage {
 	 * @param entryKeyString: Entry Protection password
 	 * @throws IOException: if FileKey is incorrect or file has been tempered
 	 */
-	
 	public void storeKey(String entryName, SecretKey secretKey, String entryKeyString) throws IOException{
 		// Obtain KeyStore
 		KeyStore keyStore = getKeyStore();
@@ -126,9 +125,9 @@ public class KeyStorage {
 	 * @param secretKey: SecretKey object to be retrieved
 	 * @param entryKeyString: Entry Protection password
 	 * @return Secret Key object if entry is found and given password is correct
-	 * @throws 	UnrecoverableEntryException: if given password is incorrect
-	 * 			NullPointerException: if no entry is found with the given name
-	 * 			IOException: if FileKey is incorrect or file has been tempered
+	 * @throws UnrecoverableEntryException: if given password is incorrect
+	 * @throws NullPointerException: if no entry is found with the given name
+	 * @throws IOException: if FileKey is incorrect or file has been tempered
 	 */
 	public SecretKey getKey(String entryName, String entryKeyString) throws UnrecoverableEntryException, NullPointerException, IOException {
 		// Get KeyStore
@@ -167,12 +166,30 @@ public class KeyStorage {
 		return entry.getSecretKey();
 	}
 
+	/* Deletes the Entry with the specified Name 
+	 * @param entryName: Alias/Name of the entry
+	 * @throws IOException: if given password is incorrect
+	 * @throws IllegalArgumentException: if entry with the given name doesn't exists
+	 */
+	public void deleteEntry(String entryName) throws IOException, IllegalArgumentException {
+		KeyStore keyStore = getKeyStore();
+		try {
+			if(!keyStore.containsAlias(entryName)) {
+				throw new IllegalArgumentException("Entry not found " + entryName);
+			}
+			keyStore.deleteEntry(entryName);
+		} catch (KeyStoreException e) {
+			// TODO Add logging statements
+			e.printStackTrace();
+		}
+	}
+	
 	/* Returns the Secret Key in string format
 	 * Obtains the SecretKey object using getKey() function and Converts it to String
 	 * 
 	 * NOTE: Do not use this function in actual code
 	 * 		 Use only for debugging operations
-	 */
+	 */	
 	public String getKeyString(String entryName, String entryKeyString) throws UnrecoverableEntryException, NullPointerException, IOException {
 		SecretKey secretKey = getKey(entryName, entryKeyString);
 		return Base64.getEncoder().encodeToString(secretKey.getEncoded());
